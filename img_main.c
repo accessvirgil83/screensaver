7d3981b5ab9ccbc191c704c8cee43028f53b5c1b
accessvirgil83 <40273814+accessvirgil83@users.noreply.github.com>
2024-02-24 23:06:33 +0500

    Create img_main.c

---
+++ img_main.c
@@ -0,0 +1,28 @@
+#include <gtk/gtk.h>
+// Author VirgilRS
+int current_image_index = 1;
+int size=1;
+
+void switch_image(GtkWidget *image) {
+    char filename[10];
+    sprintf(filename, "img/%d.jpg", current_image_index);
+    gtk_image_set_from_file(GTK_IMAGE(image), filename);
+    current_image_index = (current_image_index % size) + 1; // Переход к следующей картинке (здесь 3 - это количество изображений в карусели)
+}
+
+int main( int argc, char *argv[] ) {
+   GtkWidget *image;
+   GtkWidget *window;
+   gtk_init( &argc, &argv );
+   if(argc>1) size = atoi(argv[1]);
+   window = gtk_window_new( GTK_WINDOW_TOPLEVEL ); // Создаем главное окно
+   gtk_window_set_title( GTK_WINDOW( window ),  "Screensaver RS");
+   image = gtk_image_new();
+   gtk_window_fullscreen(GTK_WINDOW(window));   
+   gtk_container_add( GTK_CONTAINER( window ), image ); 
+   gtk_widget_show_all( window );
+   g_timeout_add_seconds(3, (GSourceFunc)switch_image, image); // Автоматическая смена картинок каждые 2 секунды   
+   g_signal_connect( G_OBJECT( window ), "destroy", G_CALLBACK( gtk_main_quit ), NULL ); // Соединяем сигнал завершения с выходом
+   gtk_main();
+   return 0;
+}
